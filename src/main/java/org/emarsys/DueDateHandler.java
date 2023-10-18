1bb794440972e6aa0bf83fc904e85c843942858f
ormosk86 <ok86@hotmail.com>
2023-10-18 17:05:13 +0200

    Log4J dependency and some logmessages added besides some code cleanup.

--- src/main/java/org/emarsys/DueDateHandler.java
+++ src/main/java/org/emarsys/DueDateHandler.java
@@ -1,62 +1,78 @@
 package org.emarsys;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.*;
 
 public class DueDateHandler {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    static final int DAY_START_HOUR = 9;
+    static final int DAY_END_HOUR = 17;
+    private static final Map<Integer, Integer> DAYS_PER_MONTH;
+
+    static {
+        DAYS_PER_MONTH = new HashMap<Integer, Integer>();
+        DAYS_PER_MONTH.put(1, 31);
+        DAYS_PER_MONTH.put(2, 28);
+        DAYS_PER_MONTH.put(3, 31);
+        DAYS_PER_MONTH.put(4, 30);
+        DAYS_PER_MONTH.put(5, 31);
+        DAYS_PER_MONTH.put(6, 30);
+        DAYS_PER_MONTH.put(7, 31);
+        DAYS_PER_MONTH.put(8, 31);
+        DAYS_PER_MONTH.put(9, 30);
+        DAYS_PER_MONTH.put(10, 31);
+        DAYS_PER_MONTH.put(11, 30);
+        DAYS_PER_MONTH.put(12, 31);
+    }
 
     int submitYear;
     int submitMonth;
     int submitDay;
     int submitHour;
     int turnAroundHours;
-    static int dayStartHour = 9;
-    static int dayEndHour = 17;
-    private static final Map<Integer, Integer> daysPerMonth;
-
-    static {
-        daysPerMonth = new HashMap<Integer,Integer>();
-        daysPerMonth.put(1,31);
-        daysPerMonth.put(2,28);
-        daysPerMonth.put(3,31);
-        daysPerMonth.put(4,30);
-        daysPerMonth.put(5,31);
-        daysPerMonth.put(6,30);
-        daysPerMonth.put(7,31);
-        daysPerMonth.put(8,31);
-        daysPerMonth.put(9,30);
-        daysPerMonth.put(10,31);
-        daysPerMonth.put(11,30);
-        daysPerMonth.put(12,31);
-    }
 
     public DueDateHandler(int submitYear, int submitMonth, int submitDay, int submitHour, int turnAroundHours) {
-        if (submitYear>0) {
+        if (submitYear > 0) {
             this.submitYear = submitYear;
-        } else {throw new IllegalArgumentException("SubmitYear parameter must not be less than 1!");}
+        } else {
+            throw new IllegalArgumentException("SubmitYear parameter must not be less than 1!");
+        }
 
         if (submitMonth < 13 && submitMonth > 0) {
             this.submitMonth = submitMonth;
-        } else {throw new IllegalArgumentException("SubmitMonth parameter must be from 1-12");}
+        } else {
+            throw new IllegalArgumentException("SubmitMonth parameter must be from 1-12");
+        }
 
-        if (submitDay>1 && submitDay<32) {
+        if (submitDay > 1 && submitDay < 32) {
             this.submitDay = submitDay;
-        } else {throw new IllegalArgumentException("SubmitDay parameter must be from 1-31");}
+        } else {
+            throw new IllegalArgumentException("SubmitDay parameter must be from 1-31");
+        }
 
-        if (submitHour<dayEndHour && submitHour>dayStartHour) {
+        if (submitHour < DAY_END_HOUR && submitHour > DAY_START_HOUR) {
             this.submitHour = submitHour;
-        } else {throw new IllegalArgumentException("SubmitHour parameter must be from 9-17");}
+        } else {
+            throw new IllegalArgumentException("SubmitHour parameter must be from 9-17");
+        }
 
-        if (turnAroundHours>0) {
+        if (turnAroundHours > 0) {
             this.turnAroundHours = turnAroundHours;
-        } else {throw new IllegalArgumentException("TurnAroundHours parameter must not be less than 1!");}
+        } else {
+            throw new IllegalArgumentException("TurnAroundHours parameter must not be less than 1!");
+        }
     }
 
     private String countEndDate(int startMonth, int startDay, int startYear, int daysToAdd) {
-        int numberOfDaysTemp = daysPerMonth.get(startMonth);
+        int numberOfDaysTemp = DAYS_PER_MONTH.get(startMonth);
 
         // For finding leap year.
         if (startMonth == 2 && startYear % 4 == 0 && startYear % 100 != 0 && startYear % 400 != 0) {
-            numberOfDaysTemp = daysPerMonth.get(startMonth) + 1;
+            numberOfDaysTemp = DAYS_PER_MONTH.get(startMonth) + 1;
         }
 
         // Calculate the updated day.
@@ -74,51 +90,49 @@ public class DueDateHandler {
                 startYear++;
             }
         }
-
         return (startMonth + "/" + updatedDay + "/" + startYear);
     }
 
-    // Implementation of the Zeller's congruence is an algorithm to calculate the day of the week for any Gregorian calendar date.
-    private int findSubmitDayOfWeek(int day, int month, int year)
-    {
-        if (month == 1)
-        {
+    // Implementation of the Zeller's congruence algorithm to calculate the day of the week for any Gregorian calendar date.
+    private int findSubmitDayOfWeek(int day, int month, int year) {
+        if (month == 1) {
             month = 13;
             year--;
         }
-        if (month == 2)
-        {
+        if (month == 2) {
             month = 14;
             year--;
         }
         int k = year % 100;
         int j = year / 100;
-        int h = day + 13*(month + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+        int h = day + 13 * (month + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+
+        logger.info("SubmitDay index " + h % 7 + " counted by the Zeller's method");
 
         return h % 7;
     }
-    public String calculateDueDate(){
+
+    public String calculateDueDate() {
         int extraDays = 0;
         int extraHours;
 
-        if (turnAroundHours > dayEndHour-submitHour){
-            extraDays ++;
-            extraDays += (turnAroundHours-(dayEndHour-submitHour))/8;
-            extraHours = (turnAroundHours-(dayEndHour-submitHour))%8;
-        }
-        else {
-            return ("Date: " + submitMonth + "/" + submitDay + "/" + submitYear)
-                    + " Hour: " + (submitHour + turnAroundHours);
+        if (turnAroundHours > DAY_END_HOUR - submitHour) {
+            extraDays++;
+            extraDays += (turnAroundHours - (DAY_END_HOUR - submitHour)) / 8;
+            extraHours = (turnAroundHours - (DAY_END_HOUR - submitHour)) % 8;
+        } else {
+            return ("Task completion DATE: " + submitMonth + "/" + submitDay + "/" + submitYear)
+                    + " HOUR: " + (submitHour + turnAroundHours);
         }
 
-        int dayOfWeek = findSubmitDayOfWeek(submitDay,submitMonth, submitYear);
+        int dayOfWeek = findSubmitDayOfWeek(submitDay, submitMonth, submitYear);
         int daysToAdd = countExtraWeekDays(extraDays, dayOfWeek);
 
-        return ("Date: " + countEndDate(submitMonth, submitDay, submitYear, daysToAdd)
-                + " Hour: " + (dayStartHour + extraHours));
+        return ("Task completion DATE: " + countEndDate(submitMonth, submitDay, submitYear, daysToAdd)
+                + " HOUR: " + (DAY_START_HOUR + extraHours));
     }
 
-    private List<String> generateWeekdays(int startIndex, int n) {
+    private List<String> generateWeekdaysList(int startIndex, int n) {
         List<String> weekdays = Arrays.asList("Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
 
         List<String> generatedWeekdays = new ArrayList<>();
@@ -133,21 +147,23 @@ public class DueDateHandler {
         return generatedWeekdays;
     }
 
-    private int countExtraWeekDays(int extraWorkingDays, int startDay){
+    private int countExtraWeekDays(int extraWorkingDays, int startDay) {
         int countWeekDays = 0;
         int extraWeekDays = 0;
-        List<String> generatedWeekdays = generateWeekdays(startDay,extraWorkingDays/7+2);
+        List<String> generatedWeekdays = generateWeekdaysList(startDay, extraWorkingDays / 7 + 2);
 
-        for (int i = 1; i < generatedWeekdays.size()-1; i++) {
+        for (int i = 1; i < generatedWeekdays.size() - 1; i++) {
             String actualDay = generatedWeekdays.get(i);
-            if (countWeekDays==extraWorkingDays){
+            if (countWeekDays == extraWorkingDays) {
                 break;
             }
-            if (!actualDay.equals("Saturday") && !actualDay.equals("Sunday")){
+            if (!actualDay.equals("Saturday") && !actualDay.equals("Sunday")) {
                 countWeekDays++;
             }
             extraWeekDays = i;
         }
+        logger.info(extraWorkingDays +" plus workingdays occurs in the upcoming " + extraWeekDays + " calendar days");
+
         return extraWeekDays;
     }
 }
--- src/main/java/org/emarsys/Main.java
+++ src/main/java/org/emarsys/Main.java
@@ -2,49 +2,27 @@ package org.emarsys;
 
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
         // Reading input by creating object of Scanner class
         Scanner sc = new Scanner(System.in);
 
-        // Display message only
-        System.out.print("Enter the submit Year in a format like '1986' : ");
-
-        // Reading String input value
-
+        System.out.print("Enter the SUBMIT YEAR in a simple number format: ");
         int submitYear = sc.nextInt();
-        //int submitYear = 2023;
 
-        // Display message only
-        System.out.print("Enter the submit Month in a format like '9': ");
-
-        // Reading String input value
+        System.out.print("Enter the SUBMIT MONTH in a simple number format: ");
         int submitMonth = sc.nextInt();
-        //int submitMonth = 10;
 
-        // Display message only
-        System.out.print("Enter the submit Day in a format like '4': ");
-
-        // Reading String input value
+        System.out.print("Enter the SUBMIT DAY in a simple number format: ");
         int submitDay = sc.nextInt();
-        //int submitDay = 20;
 
-        System.out.print("Enter the submit working hour in a format like '15' between 9-17: ");
-
-        // Reading String input value
+        System.out.print("Enter the SUBMIT WORKING HOUR in a simple number format between 9-17: ");
         int submitHour = sc.nextInt();
-        //int submitHour = 10;
 
-        System.out.print("Enter the turnaround time in hours in a format like '75': ");
-
-        // Reading String input value
+        System.out.print("Enter the TURNAROUND TIME in hours in a simple number format: ");
         int turnAroundHours = sc.nextInt();
-        //int turnAroundHours = 3;
 
         System.out.println(new DueDateHandler(submitYear,submitMonth,submitDay,submitHour,turnAroundHours).calculateDueDate());
-        //System.out.println(new DueDateHandler(submitYear,submitMonth,submitDay,submitHour,turnAroundHours).countExtraWeekDays(2,6,2));
     }
 }
